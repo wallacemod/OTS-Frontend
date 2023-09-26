4e11f4aa5fe0a99274ebdd52686fdd47e3f423cc
wallacemod <wallacepatos@hotmail.com>
2023-09-25 21:57:07 -0300

    Criado Card 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,15 @@ function App() {
     imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
 
+  const item2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
   return (
     <>
       <Card item={item1}/>
+      <Card item={item2}/>
     </>
   )
 }
