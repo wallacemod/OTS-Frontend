7f30656d1d3c325870628a4e41aaf0b2a27a0243
wallacemod <wallacepatos@hotmail.com>
2023-09-25 21:50:09 -0300

    Adicionando propriedades ao componente Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,18 @@
 import "./Card.css"
 
-export default function Card() {
+export default function Card(props) {
+    const item = props.item;
+
+
     return <div className="card">
-    <h2>Rick Sanchez</h2>
+      <h2>{item.nome}</h2>
 
-    <div className="tags">
-      <div className="tag">Status: Vivo</div>
-      <div className="tag">Espécie: Humana</div>
-      <div className="tag">Origem: Terra C-137</div>
+      <div className="tags">
+        <div className="tag">Status: Vivo</div>
+        <div className="tag">Espécie: Humana</div>
+        <div className="tag">Origem: Terra C-137</div>
+      </div>
+
+      <img src={item.imagemUrl}/>
     </div>
-
-    <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
-  </div>
 }
